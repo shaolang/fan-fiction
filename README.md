dc7ec52c20f445d7d3180c76b6b008a2db2a5fa3
shaolang <shaolang@ymail.com>
2021-03-18 22:26:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# fan-fiction
+
+Write [Storybook][storybook] in ClojureScript with ease.
+
+## License
+
+Copyright © 2021 Shaolang Ai
+
+Distributed under the MIT License
+
+[storybook]: https://storybook.js.org
---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject fan-fiction "0.1.0-SNAPSHOT"
+  :description "Write Storybook in ClojureScript with ease"
+  :url "https://github.com/shaolang/fan-fiction"
+  :license {:name "MIT License"
+            :url "https://mit-license.org"}
+
+  :min-lein-version "2.7.1"
+
+  :source-paths ["src"]
+
+  :aliases {"fig"       ["run" "-m" "figwheel.main"]
+            "fig:build" ["run" "-m" "figwheel.main" "-b" "dev" "-r"]}
+
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main  "0.2.12"]
+                                  [org.clojure/clojure        "1.10.3"
+                                   :scope "provided"]
+                                  [org.clojure/clojurescript  "1.10.773"
+                                   :scope "provided"]
+                                  [reagent                    "1.0.0"
+                                   :scope "provided"]]
+                   :resource-paths ["target"]
+                   :clean-targets ^{:protect false} ["target"]}}
+
+  :scm {:name "git"
+        :url  "https://github.com/shaolang/fan-fiction"})
