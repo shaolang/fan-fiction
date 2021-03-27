f1f643f076913926cea3550ebd6370c0543235c6
shaolang <shaolang@ymail.com>
2021-03-27 15:50:12 +0800

    Add test to defstory getting a component as a function

--- test/fan-fiction/reagent_test.cljs
+++ test/fan-fiction/reagent_test.cljs
@@ -1,11 +1,16 @@
 (ns fan-fiction.reagent-test
   (:require [cljs.test :refer [are]]
             [devcards.core :refer [deftest]]
-            [fan-fiction.reagent :refer [defstory]]))
+            [fan-fiction.reagent :refer [defstory]]
+            reagent.core))
 
-(deftest defstory-test
+(deftest defstory-macro-expansions
   (are [input expected] (= (macroexpand input)
                            expected)
 
        '(defstory hello [:h1 "World"])
-       '(def hello (clojure.core/fn [] (reagent.core/as-element [:h1 "World"])))))
+       '(def hello (clojure.core/fn [] (reagent.core/as-element [:h1 "World"])))
+
+       '(defstory hi (fn [] [:h1 "Sekai"]))
+       '(def hi (clojure.core/fn []
+                  (reagent.core/as-element [(fn [] [:h1 "Sekai"])])))))
