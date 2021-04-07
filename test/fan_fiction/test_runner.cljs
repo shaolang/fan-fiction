88ad70d5c9cd55876270c0eb230926cfeef3b2f6
shaolang <shaolang@ymail.com>
2021-04-07 20:03:47 +0800

    Fix error in test directory name

--- test/fan_fiction/test_runner.cljs
+++ test/fan_fiction/test_runner.cljs
@@ -1,5 +1,6 @@
 (ns fan-fiction.test-runner
   (:require [devcards.core]
-            fan-fiction.reagent-test))
+            fan-fiction.reagent-test
+            fan-fiction.utils-test))
 
 (devcards.core/start-devcard-ui!)
